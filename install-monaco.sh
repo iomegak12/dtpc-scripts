8ca3590180fde4a38dd07199ce75230f23629472
iomegak12 <jd.ramkumar@gmail.com>
2022-05-09 08:07:05 +0530

    commit

--- install-monaco.sh
+++ install-monaco.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-curl -L https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/v1.7.0/monaco-linux-amd64 -o monaco
+curl -L https://github.com/dynatrace-oss/dynatrace-monitoring-as-code/releases/download/v1.7.0/monaco-linux-amd64 -o ~/monaco
 
-chmod +x monaco
+chmod +x ~/monaco
 
-sudo mv monaco /usr/local/bin/
+sudo mv ~/monaco /usr/local/bin/
 
 monaco --help
